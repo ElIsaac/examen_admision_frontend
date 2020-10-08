0b2db14d78547f860cfe158da7becae7e3c1e340
Isaac ramirez velez <ramirezisaac.1518@gmail.com>
2020-10-08 02:52:41 -0600

    Updates

---
+++ precache-manifest.bf6898fc0e0e1373c630094c73d949dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d260fb7157e13fdcce7311897f994a",
+    "url": "/examen_admision_frontend/index.html"
+  },
+  {
+    "revision": "88279e9f1d9724f546f0",
+    "url": "/examen_admision_frontend/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "664f67e643c3681561cd",
+    "url": "/examen_admision_frontend/static/css/main.4a386d1f.chunk.css"
+  },
+  {
+    "revision": "88279e9f1d9724f546f0",
+    "url": "/examen_admision_frontend/static/js/2.b0a781cf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/examen_admision_frontend/static/js/2.b0a781cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "664f67e643c3681561cd",
+    "url": "/examen_admision_frontend/static/js/main.f2723083.chunk.js"
+  },
+  {
+    "revision": "dc5c9b4cd1e3f189ad46",
+    "url": "/examen_admision_frontend/static/js/runtime-main.d2f48b7d.js"
+  }
+]);
